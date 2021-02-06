61d512c3c29a078379155f319b2c3e8be73f8dca
venambati <v_ambati@yahoo.com>
2021-02-06 18:26:02 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Optum Docs`,
     pathPrefix: "/optum-docs",
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Venkat Ambati`,
+      summary: `who lives and works in Raleigh.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
