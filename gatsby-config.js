32d3d16a98b2d923c6963196663b659dfa286cdc
vambati1 <venkat.ambati@optum.com>
2021-02-06 16:55:42 -0500

    path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
+    pathPrefix: "/optum-docs",
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
